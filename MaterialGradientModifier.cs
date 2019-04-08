5f2abbb572ec7e9a6fd556c0953e6e176bd46127
kutar777 <tabi_kutar@yahoo.co.jp>
2019-04-08 23:59:12 +0900

    Add files via upload

---
+++ MaterialGradientModifier.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Renderer))]
+public class MaterialGradientModifier : MonoBehaviour
+{
+
+    Renderer _renderer;
+
+    //public Color myColor;
+    [SerializeField] Gradient gradient;
+
+    float _gradientPosition = -1;
+    public float gradientPosition
+    {
+        get { return _gradientPosition; }
+        set
+        {
+            if (_gradientPosition != value)
+            {
+                _gradientPosition = value;
+                _renderer.material.color = gradient.Evaluate(_gradientPosition);
+            }
+
+        }
+    }
+
+
+    //void SetGradientPosition (float position)
+    //{
+    //    if(position == gradientPosition)
+    //        return;
+
+    //    gradientPosition = position;
+    //    _renderer.material.color = gradient.Evaluate(gradientPosition);
+        
+    //}
+
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        
+    }
+
+    //void Start()
+    //{
+    //    gradientPosition = 0;
+
+    //}
+
+    //void Update()
+    //{
+    //    gradientPosition = Mathf.Sin((Time.time) * 0.5f) + 0.5f;
+    //    //SetGradientPosition(Mathf.Sin((Time.time)*0.5f) + 0.5f);
+    //}
+}
