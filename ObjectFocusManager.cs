8e55dd0a8d420c1e55daf379c5a8ab0bfbfe135c
kutar777 <tabi_kutar@yahoo.co.jp>
2019-10-08 01:19:31 +0900

    Add files via upload

---
+++ ObjectFocusManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocusManager : MonoBehaviour {
+
+    private static ObjectFocusManager _instance;
+    public static ObjectFocusManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<ObjectFocusManager>();
+                if(_instance == null)
+                {
+                    _instance = (new GameObject("Singleton")).AddComponent<ObjectFocusManager>();
+                }
+            }
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+    void Awake()
+    {
+        if (Instance && Instance != this)
+            Destroy(gameObject);
+        Instance = this;
+    }
+    private void OnDisable()
+    {
+        Instance = null;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
