c957a09fd78220c65c4f45f78ab2aec85ca2b1ea
kutar777 <tabi_kutar@yahoo.co.jp>
2020-09-28 00:50:36 +0900

    Add files via upload

---
+++ ButtonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using System;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject scrollView;
+
+    public BoolReactiveProperty IsExpanded = new BoolReactiveProperty(false);
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.OnClickAsObservable()
+            .Subscribe(x => IsExpanded.Value = !IsExpanded.Value);
+    }
+
+    private void Start()
+    {
+        IsExpanded.Subscribe(ButtonHandler);
+    }
+
+    private void ButtonHandler(bool flag)
+    {
+        if (flag)
+        {
+            var buttons = transform.parent.gameObject.GetComponentsInChildren<ButtonController>();
+            foreach (var btn in buttons)
+            {
+                if (btn.button != button && btn.IsExpanded.Value == true)
+                    btn.IsExpanded.Value = false;
+            }
+
+            scrollView.transform.SetSiblingIndex(transform.GetSiblingIndex() + 1);
+            scrollView.SetActive(true);
+            print("BT Sibling Index: " + transform.GetSiblingIndex());
+            print("SV Sibling Index: " + scrollView.transform.GetSiblingIndex());
+        }
+        else
+            scrollView.SetActive(false);
+    }
+}
---
+++ ScrollController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class ScrollController : MonoBehaviour
+{
+    private Scrollbar scrollbar;
+    private ButtonController[] buttons;
+
+    [SerializeField]
+    [Range(0, 1f)]
+    private float sliderValue;
+
+    [SerializeField]
+    private float buttonHeightRate = 0f;
+
+    [SerializeField]
+    private GameObject scrollView;
+
+    private void Awake()
+    {
+        scrollbar = GetComponent<ScrollRect>().verticalScrollbar;
+        buttons = GetComponentsInChildren<ButtonController>();
+        
+    }
+
+    private void Start()
+    {
+        foreach (var btn in buttons)
+        {
+            btn.IsExpanded
+                .Where(_ => true)
+                .Subscribe(x =>
+                {
+                    var btnIndex = btn.transform.GetSiblingIndex();
+                    var svIndex = scrollView.transform.GetSiblingIndex();
+
+                    if(svIndex < btnIndex)
+                        sliderValue = 1f - (btnIndex - 1)  * buttonHeightRate;
+                    else
+                        sliderValue = 1f - btnIndex * buttonHeightRate;
+
+                    scrollbar.value = sliderValue;
+                });
+        }
+        scrollbar.value = 1f;
+        var rect = buttons[0].GetComponent<RectTransform>();
+        buttonHeightRate = rect.sizeDelta.y / GetComponent<RectTransform>().sizeDelta.y;
+    }
+
+    private void Update()
+    {
+        //scrollbar.value = sliderValue;
+    }
+}
